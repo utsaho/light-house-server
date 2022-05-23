0b8146773a1768d0311c072aa687f7975412fcf3
utsho <utsho111099@gmail.com>
2022-05-23 16:48:42 +0600

    setting up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -5,6 +6,27 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nkmib.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async () => {
+    try {
+        await client.connect();
+        const userCollection = client.db('light-house').collection('users');
+
+        app.get('/test', async (req, res) => {
+            res.send({ message: 'hello there' });
+        });
+    }
+    finally { };
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
