ca01fb75f2f4f8ddadac67d458a810132873804f
utsho <utsho111099@gmail.com>
2022-05-31 23:44:49 +0600

    Purches page added

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ const run = async () => {
             res.send(result);
         });
 
+        //* Getting services expect single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await serviceCollection.find({}) /* use skip and size */.toArray();
+            res.send(result);
+        });
+
     }
     finally { };
 }
